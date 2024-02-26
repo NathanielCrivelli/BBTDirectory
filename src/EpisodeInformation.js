880eac9a749bcb6f3fe0839ee369ab216dd418ad
Nathaniel Crivelli <nathaniel.crivelli@mrjaffesclass.com>
2024-02-26 14:20:24 -0800

    done

--- src/EpisodeInformation.js
+++ src/EpisodeInformation.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
 function EpisodeInformation(props) {
-    
+
+    const removeEnds = function(text) {
+      let result = text.substring(3, text.length - 4);
+      return result;
+    }
+
     return (
       <div className='Episode-Information'>
         <h4 className='Episode-Name-Box'>
             Episode Name: {!props.ep ? "Loading..." : props.ep.name}
         </h4>
         <div className='Episode-Information-Box'>
-            Info: {!props.ep ? "Loading..." : props.ep.summary}
+            Info: {!props.ep ? "Loading..." : removeEnds(props.ep.summary)}
         </div>
-        <div className='Episode-Description-Box'>
-            Description: {}
+        <div className='Episode-Extras-Box'>
+            Extra: {!props.ep ? "Loading..." : props.ep.airstamp}
         </div>
-        <img src={!props.ep ? "Loading..." : props.ep.url} alt=''/>
+        <img src={!props.ep ? "" : props.ep.image.medium} alt='Loading...'/>
       </div>
     );
   }
